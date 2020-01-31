9820c861233f09c5fd93d76de7c5ea8be83a4eab
knmac <khoinguyenx@gmail.com>
2020-01-30 18:46:45 -0600

    add neomuttrc

--- install.sh
+++ install.sh
@@ -46,6 +46,19 @@ cp -r ./oh-my-zsh/custom $HOME/.oh-my-zsh/custom
 
 
 # -----------------------------------------------------------------------------
+echo "Install and setup W3M and NeoMutt"
+sudo apt-get install w3m w3m-img
+sudo apt-get install neomutt
+
+mkdir -p $HOME/.config/neomutt
+mkdir -p $HOME/.config/neomutt/cache
+touch $HOME/.config/neomutt/certificates
+cp -r neomutt/* $HOME/.config/neomutt
+
+echo "You need to change the user name in $HOME/.config/neomutt/neomuttrc later..."
+
+
+# -----------------------------------------------------------------------------
 echo "Install Powerline font"
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
