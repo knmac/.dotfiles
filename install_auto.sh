8f0c23542a572ad418f5a827f2e365908ee82ee3
knmac <khoinguyenx@gmail.com>
2020-04-26 14:31:07 -0500

    update

--- install_auto.sh
+++ install_auto.sh
@@ -40,7 +40,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 cp zsh/.zshrc "$HOME"
 
 # Install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting "
+git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 
 # Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
