1399aa1fa26068f1fa88dc9ac19c304a188ef90d
knmac <khoinguyenx@gmail.com>
2020-05-11 09:56:13 -0500

    update

--- install_auto.sh
+++ install_auto.sh
@@ -4,19 +4,20 @@ echo "Automatically install and set up the packages. Requires sudo"
 # -----------------------------------------------------------------------------
 echo "Install and setup NeoVim"
 sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install neovim
+sudo apt update
+sudo apt install neovim
 
 #sudo apt-get install python-dev python-pip python3-dev python3-pip curl vim exuberant-ctags git ack-grep
 #pip install --user neovim pep8 flake8 pyflakes pylint isort pynvim
 #pip3 install --user neovim pep8 flake8 pyflakes pylint isort pynvim
-sudo apt-get install python-dev python-pip python3-dev python3-pip curl exuberant-ctags
-pip3 install --user pynvim flake8
+sudo apt install python3-dev python3-pip curl exuberant-ctags
+pip3 install --user pynvim neovim flake8 msgpack
 
 mkdir -p "$HOME/.config/nvim/"
-cp ./nvim/init.vim ./nvim/skeleton.py "$HOME/.config/nvim"
+cp -r ./nvim/* "$HOME/.config/nvim"
 
-echo "Run nvim to begin the setup..."
+echo "Run nvim and begin the setup..."
+nvim
 
 
 # -----------------------------------------------------------------------------
@@ -46,10 +47,13 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plug
 git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 
 # Install bat for syntax highlighting
-sudo apt-get install bat
+sudo apt install bat
 
 # Install ripgrep
-sudo apt-get install ripgrep
+sudo apt install ripgrep
+
+# Install shellcheck
+sudo apt install shellcheck
 
 # Copy the settings over
 #cp oh-my-zsh/custom/themes/custom-bullet.zsh-theme ~/.oh-my-zsh/custom/themes 
