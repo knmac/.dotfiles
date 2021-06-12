9d5402d05238e66252b8fe3dcdf6e5de1d3aa6fb
knmac <khoinguyenx@gmail.com>
2021-06-11 21:56:51 -0500

    update

--- tiling_wm/polybar/scripts/powermenu.sh
+++ tiling_wm/polybar/scripts/powermenu.sh
@@ -22,16 +22,16 @@ logout="  Logout"
 
 # Confirmation
 confirm_exit() {
-	rofi -dmenu\
-		-i\
-		-no-fixed-num-lines\
-		-p "Are You Sure? : "\
-		-theme $rofi_theme
+    rofi -dmenu\
+        -i\
+        -no-fixed-num-lines\
+        -p "Are You Sure? : "\
+        -theme $rofi_theme
 }
 
 # Message
 msg() {
-	rofi -theme "$rofi_theme" -e "Available Options  -  yes / y / no / n"
+    rofi -theme "$rofi_theme" -e "Available Options  -  yes / y / no / n"
 }
 
 # Variable passed to rofi
@@ -40,59 +40,59 @@ options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
 chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -i -selected-row 0)"
 case $chosen in
     $shutdown)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			systemctl poweroff
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
+        ans=$(confirm_exit &)
+        if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+            systemctl poweroff
+        elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+            exit 0
         else
-			msg
+            msg
         fi
         ;;
     $reboot)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			systemctl reboot
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
+        ans=$(confirm_exit &)
+        if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+            systemctl reboot
+        elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+            exit 0
         else
-			msg
+            msg
         fi
         ;;
     $lock)
-		#if [[ -f /usr/bin/i3lock ]]; then
-		#    i3lock
-		#elif [[ -f /usr/bin/betterlockscreen ]]; then
-		#    betterlockscreen -l
-		#fi
+        #if [[ -f /usr/bin/i3lock ]]; then
+        #    i3lock
+        #elif [[ -f /usr/bin/betterlockscreen ]]; then
+        #    betterlockscreen -l
+        #fi
         ~/.config/i3/i3lock.sh
         ;;
     $suspend)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			mpc -q pause
-			#amixer set Master mute
-			systemctl suspend
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
+        ans=$(confirm_exit &)
+        if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+            mpc -q pause
+            #amixer set Master mute
+            systemctl suspend
+        elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+            exit 0
         else
-			msg
+            msg
         fi
         ;;
     $logout)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
-				i3-msg exit
-			fi
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
+        ans=$(confirm_exit &)
+        if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+            if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+                openbox --exit
+            elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+                bspc quit
+            elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+                i3-msg exit
+            fi
+        elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+            exit 0
         else
-			msg
+            msg
         fi
         ;;
 esac
