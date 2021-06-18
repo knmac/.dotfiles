00704690e3b19f7afa0f6a8bb00d548ed77cda1f
knmac <khoinguyenx@gmail.com>
2021-06-17 22:59:52 -0500

    update

---
+++ tiling_wm/polybar/scripts/find_keybindings.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+KEY_BINDINGS="$HOME/.config/polybar/scripts/keybindings.csv"
+
+parse_keys() {
+    awk 'BEGIN {FS = ","}; {printf("<b>%-35s</b>%s\n", $1, $2)} ' $KEY_BINDINGS
+}
+
+ans=$( parse_keys | rofi -i -dmenu -p "  Key bindings" -markup-rows )
+
+echo $ans | awk 'BEGIN {FS = "</b>"}; {print $2}'
