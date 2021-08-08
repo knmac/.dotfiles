b929ebe009b9133def10ca2411be0b2d60d9ccc8
knmac <khoinguyenx@gmail.com>
2021-08-08 16:40:32 -0500

    update

--- tiling_wm/.config/polybar/launch.sh
+++ tiling_wm/.config/polybar/launch.sh
@@ -10,7 +10,16 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 #polybar --reload mybar &
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload mybar &
+        # get the screen rotation
+        rot=$(xrandr --verbose | grep $m | egrep -o " (normal) ")
+        # select the bar name wrt rotation
+        if [[ $rot == " normal " ]]; then
+            bar_name="mybar"
+        else
+            bar_name="mybar_vert"
+        fi
+        # launch the bar on the correct monitor
+        MONITOR=$m polybar --reload $bar_name &
     done
 else
     polybar --reload mybar &
