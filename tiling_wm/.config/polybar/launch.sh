52c5293e18fe08a7661eca56f3e0a35299cfffce
knmac <khoinguyenx@gmail.com>
2021-08-08 15:55:28 -0500

    update

--- tiling_wm/.config/polybar/launch.sh
+++ tiling_wm/.config/polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-#polybar mybar &
+#polybar --reload mybar &
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         MONITOR=$m polybar --reload mybar &
