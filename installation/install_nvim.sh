10940b96811d5ca8d719258af924157b546845d5
knmac <khoinguyenx@gmail.com>
2021-10-07 22:10:18 -0500

    update

--- installation/install_nvim.sh
+++ installation/install_nvim.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
     NVIM_VERSION="v0.5.0"
 else
     NVIM_VERSION=$1
@@ -10,17 +10,17 @@ echo "Install NeoVim..."
 
 # Dependecies
 bash ./install_nodejs.sh
-sudo apt install python3-dev python3-pip curl exuberant-ctags shellcheck
+sudo apt install python3-dev python3-pip curl exuberant-ctags shellcheck cppcheck
 pip3 install --user pynvim neovim flake8 msgpack pycodestyle
 
 # Download nvim
 wget "https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/nvim-linux64.tar.gz"
 tar zxvf nvim-linux64.tar.gz
-cp -r nvim-linux64/* $HOME/.local
+cp -r nvim-linux64/* "$HOME"/.local
 rm -r nvim-linux64
 rm nvim-linux64.tar.gz
 
 # Get config
-pushd $DOTFILES_ROOT
+pushd "$DOTFILES_ROOT" || exit
 stow nvim
-popd
+popd || exit
