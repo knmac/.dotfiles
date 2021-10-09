09faf2c2aa1b53905d4baaa34ef838def5b8b3d9
knmac <khoinguyenx@gmail.com>
2021-10-08 19:19:28 -0500

    update

--- nvim/.config/nvim/plugin_cfg/lua/lint.lua
+++ nvim/.config/nvim/plugin_cfg/lua/lint.lua
@@ -1,5 +1,8 @@
 require('lint').linters_by_ft = {
   python = {'pycodestyle',},  -- check PEP8 style: pip install pycodestyle
+  cpp = {'cppcheck',},
+  c = {'cppcheck'},
+  sh = {'shellcheck'},
 }
 
 vim.cmd([[ au BufEnter * lua require('lint').try_lint() ]])
