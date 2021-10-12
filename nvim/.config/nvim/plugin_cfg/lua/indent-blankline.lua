7c393ef8a8ebb94c9a2bb3798e664dcd14d6b449
knmac <khoinguyenx@gmail.com>
2021-10-12 17:19:17 -0500

    update

--- nvim/.config/nvim/plugin_cfg/lua/indent-blankline.lua
+++ nvim/.config/nvim/plugin_cfg/lua/indent-blankline.lua
@@ -1,6 +1,8 @@
+vim.opt.list = true
 require("indent_blankline").setup {
     --char = "|",
     buftype_exclude = {"terminal"},
-    filetype_exclude = {"help", "startify", "make", "NvimTree"},
+    filetype_exclude = {"help", "startify", "make", "NvimTree", "dashboard"},
     use_treesitter = true,
+    --show_current_context = true,
 }
