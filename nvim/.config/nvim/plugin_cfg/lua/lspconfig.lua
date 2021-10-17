12b44dc0ffd1f4f581e6fb1a7d4c4c7ac5aa9bcf
knmac <khoinguyenx@gmail.com>
2021-10-16 22:17:22 -0500

    update

--- nvim/.config/nvim/plugin_cfg/lua/lspconfig.lua
+++ nvim/.config/nvim/plugin_cfg/lua/lspconfig.lua
@@ -2,7 +2,7 @@ local nvim_lsp = require('lspconfig')
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
@@ -44,12 +44,14 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 -- Run :LspInstall <server> to install, require 'nvim-lsp-installer'
-local servers = {'pyright', 'bashls', 'clangd', 'dockerls', 'texlab', 'vimls', 'sumneko_lua'}
-for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup {
-        on_attach = on_attach,
-        flags = {
-            debounce_text_changes = 150,
-        }
-    }
-end
+--local servers = {'pyright', 'bashls', 'clangd', 'dockerls', 'texlab', 'vimls', 'sumneko_lua'}
+--for _, lsp in ipairs(servers) do
+--    nvim_lsp[lsp].setup {
+--        on_attach = on_attach,
+--        flags = {
+--            debounce_text_changes = 150,
+--        }
+--    }
+--end
+
+on_attach()
