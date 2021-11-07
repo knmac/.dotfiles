3de315786eb8d95f7086d41dc3a5cb3844b9727f
knmac <khoinguyenx@gmail.com>
2021-11-07 10:35:01 -0800

    update

--- nvim/.config/nvim/plugin_cfg/lua/treesitter.lua
+++ nvim/.config/nvim/plugin_cfg/lua/treesitter.lua
@@ -1,28 +1,28 @@
 require'nvim-treesitter.configs'.setup {
-  highlight = {
-    enable = true,
-    disable = {},
-  },
-  indent = {
-    enable = false,  -- treesitter's indent is buggy
-    disable = {},
-  },
-  ensure_installed = {
-    "python",
-    "c",
-    "cpp",
-    "bash",
-    "latex",
-    "bibtex",
-    "json",
-    "yaml",
-    "html",
-    "css",
-    "lua",
-    "cmake",
-    "dockerfile"
-  },
-  matchup = {
-    enable = true,
-  },
+    highlight = {
+        enable = true,
+        disable = {},
+    },
+    indent = {
+        enable = false,  -- treesitter's indent is buggy
+        disable = {},
+    },
+    ensure_installed = {
+        "python",
+        "c",
+        "cpp",
+        "bash",
+        "latex",
+        "bibtex",
+        "json",
+        "yaml",
+        "html",
+        "css",
+        "lua",
+        "cmake",
+        "dockerfile"
+    },
+    matchup = {
+        enable = true,
+    },
 }
