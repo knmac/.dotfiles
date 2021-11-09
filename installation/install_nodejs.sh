0ba03922f50b26b6544c05a0f31ac728944aa3e3
knmac <khoinguyenx@gmail.com>
2021-11-09 08:14:10 -0800

    update

--- installation/install_nodejs.sh
+++ installation/install_nodejs.sh
@@ -9,7 +9,7 @@ echo "$NODE_VERSION"
 echo "Install and update NodeJS..."
 wget "https://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}-linux-x64.tar.xz"
 tar xvf "node-${NODE_VERSION}-linux-x64.tar.xz" -C "$HOME/.local"
-rm "node-${NODE_VERSION}-linux-x64.tar.xz "
+rm "node-${NODE_VERSION}-linux-x64.tar.xz"
 
 unlink "$HOME/.local/nodejs"
 ln -sf "$HOME/.local/node-${NODE_VERSION}-linux-x64" "$HOME/.local/nodejs"
