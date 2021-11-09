67a08c83cfdb0fe32ac7867c2597f5ba04e75576
knmac <khoinguyenx@gmail.com>
2021-11-09 08:13:13 -0800

    update

--- installation/install_nodejs.sh
+++ installation/install_nodejs.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-if [ -z $1 ]; then
-    NODE_VERSION="v16.4.0"
+if [ -z "$1" ]; then
+    NODE_VERSION="v17.0.1"
 else
-    NODE_VERSION=$1
+    NODE_VERSION="$1"
 fi
-echo $NODE_VERSION
+echo "$NODE_VERSION"
 
 echo "Install and update NodeJS..."
 wget "https://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}-linux-x64.tar.xz"
-tar xvf node-${NODE_VERSION}-linux-x64.tar.xz -C $HOME/.local
-rm node-${NODE_VERSION}-linux-x64.tar.xz 
+tar xvf "node-${NODE_VERSION}-linux-x64.tar.xz" -C "$HOME/.local"
+rm "node-${NODE_VERSION}-linux-x64.tar.xz "
 
-ln -sf $HOME/.local/node-${NODE_VERSION}-linux-x64 $HOME/.local/nodejs
+unlink "$HOME/.local/nodejs"
+ln -sf "$HOME/.local/node-${NODE_VERSION}-linux-x64" "$HOME/.local/nodejs"
 echo "Remember to add ~/.local/nodejs/bin to your \$PATH"
