bf0c8cc88464c7d662704bd7211698333efc75f0
knmac <khoinguyenx@gmail.com>
2021-11-29 15:21:55 -0800

    update

--- installation/install_zsh.sh
+++ installation/install_zsh.sh
@@ -5,7 +5,7 @@ echo "Automatically install and set up the packages. Requires sudo"
 
 # -----------------------------------------------------------------------------
 echo "Install and setup zsh"
-sudo apt install zsh
+sudo apt install zsh fzf
 
 # Clone oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
