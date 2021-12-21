278c61ad6c1aa54871e35646c7646c0bfa4d7f90
knmac <khoinguyenx@gmail.com>
2021-12-21 09:53:09 -0800

    update

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -26,6 +26,9 @@ map('n', 'k', 'gk', default_opts)
 -- map('n', 'j', 'jzz', default_opts)
 -- map('n', 'k', 'kzz', default_opts)
 
+-- Use ; to enter command mode
+map('n', ';', ':', { noremap = true })
+map('v', ';', ':', { noremap = true })
 
 -- Window navigation
 map('n', '<A-h>', '<C-w>h', default_opts)
