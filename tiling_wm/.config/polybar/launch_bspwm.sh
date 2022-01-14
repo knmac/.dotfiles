289fdc8a89db80d49f4da0c7954654363d2a7870
knmac <khoinguyenx@gmail.com>
2022-01-13 22:13:33 -0800

    split config for bspwm

--- tiling_wm/.config/polybar/launch_bspwm.sh
+++ tiling_wm/.config/polybar/launch_bspwm.sh
@@ -6,11 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
+# Launch Polybar, using config location ~/.config/polybar/config_bspwm
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         # get the screen rotation
-        rot=$(xrandr --verbose | grep $m | egrep -o " (normal) ")
+        rot=$(xrandr --verbose | grep "$m" | grep -E -o " (normal) ")
         # select the bar name wrt rotation
         if [[ $rot == " normal " ]]; then
             bar_name="mybar"
@@ -18,10 +18,10 @@ if type "xrandr"; then
             bar_name="mybar_vert"
         fi
         # launch the bar on the correct monitor
-        MONITOR=$m polybar --reload $bar_name &
+        MONITOR=$m polybar --reload $bar_name --config="$HOME/.config/polybar/config_bspwm" &
     done
 else
-    polybar --reload mybar &
+    polybar --reload mybar --config="$HOME/.config/polybar/config_bspwm" &
 fi
 
 echo "Polybar launched..."
