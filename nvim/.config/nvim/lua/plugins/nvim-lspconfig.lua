f38652cd7f4ecdcd1ea5a8e0b510cd64742ca9f4
knmac <khoinguyenx@gmail.com>
2022-02-03 16:17:36 -0800

    update

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -59,19 +59,19 @@ local on_attach = function(client, bufnr)
         buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
         buf_set_keymap('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
 
-        buf_set_keymap('n', '<F9>', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+        buf_set_keymap('n', '<F9>', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
     end
 
-    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gD',    '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'K',     '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
     buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 
     buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
     buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-    buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "single" })<CR>', opts)
-    buf_set_keymap('n', '[e', '<cmd>lua vim.lsp.diagnostic.goto_prev({ float = { border = "single" }})<CR>', opts)
-    buf_set_keymap('n', ']e', '<cmd>lua vim.lsp.diagnostic.goto_next({ float = { border = "single" }})<CR>', opts)
+    buf_set_keymap('n', '<leader>f',  '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    buf_set_keymap('n', '<leader>e',  '<cmd>lua vim.diagnostic.open_loat({ border = "single" })<CR>', opts)
+    buf_set_keymap('n', '[e',         '<cmd>lua vim.diagnostic.goto_prev({ float = { border = "single" }})<CR>', opts)
+    buf_set_keymap('n', ']e',         '<cmd>lua vim.diagnostic.goto_next({ float = { border = "single" }})<CR>', opts)
 
     -- buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
     -- buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
@@ -102,7 +102,7 @@ lsp_setup_opts['sumneko_lua'] = {
     settings = {
         Lua = {
             diagnostics = {
-                globals = { 'vim', 'use' }
+                globals = { 'vim', 'use' }  -- global variables to ignore diagnostic
             }
         }
     }
