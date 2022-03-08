4604f2aab572be55e3cda69fef12526e82eb4564
Khoi-Nguyen C. Mac <knmac@users.noreply.github.com>
2022-03-08 15:43:35 -0800

    Update nvim-tree.lua

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -92,7 +92,7 @@ require('nvim-tree').setup({
     -- will not open on setup if the filetype is in this list
     ignore_ft_on_setup  = {'startify', 'dashboard', 'alpha',},
     -- closes neovim automatically when the tree is the last **WINDOW** in the view
-    auto_close          = true,
+    auto_close          = false,
     -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
     open_on_tab         = false,
     -- hijack the cursor in the tree to put it at the start of the filename
