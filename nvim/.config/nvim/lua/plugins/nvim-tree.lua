bc27c1677a6d4c58c8e5368b07dab9e6bb9c0a92
knmac <khoinguyenx@gmail.com>
2022-04-10 22:06:55 -0700

    update

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -7,7 +7,7 @@ local cmd = vim.cmd
 
 -- g.nvim_tree_gitignore = 1 -- 0 by default
 -- g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
-g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
+-- g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
 g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
 g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
 g.nvim_tree_root_folder_modifier = ':~' -- This is the default. See :help filename-modifiers for more options
@@ -82,98 +82,111 @@ g.nvim_tree_icons = {
 -- autocmd ColorScheme * highlight NvimTreeFolderIcon guibg=blue
 -- ]]
 
-require('nvim-tree').setup({
-    -- disables netrw completely
-    disable_netrw       = true,
-    -- hijack netrw window on startup
-    hijack_netrw        = true,
-    -- open the tree when running this setup function
-    open_on_setup       = false,
-    -- will not open on setup if the filetype is in this list
-    ignore_ft_on_setup  = {'startify', 'dashboard', 'alpha',},
-    -- closes neovim automatically when the tree is the last **WINDOW** in the view
-    auto_close          = false,
-    -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-    open_on_tab         = false,
-    -- hijack the cursor in the tree to put it at the start of the filename
-    hijack_cursor       = false,
-    -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-    update_cwd          = true,
-    update_to_buf_dir = {
+-- each of these are documented in `:help nvim-tree.OPTION_NAME`
+require'nvim-tree'.setup {
+    auto_reload_on_write = true,
+    disable_netrw = false,
+    hide_root_folder = false,
+    hijack_cursor = false,
+    hijack_netrw = true,
+    hijack_unnamed_buffer_when_opening = false,
+    ignore_buffer_on_setup = false,
+    open_on_setup = false,
+    open_on_setup_file = false,
+    open_on_tab = false,
+    sort_by = "name",
+    update_cwd = false,
+    view = {
+        width = 30,
+        height = 30,
+        side = "left",
+        preserve_window_proportions = false,
+        number = false,
+        relativenumber = false,
+        signcolumn = "yes",
+        mappings = {
+            custom_only = false,
+            list = {
+                -- user mappings go here
+            },
+        },
+    },
+    renderer = {
+        indent_markers = {
+            enable = true,
+            icons = {
+                corner = "└ ",
+                edge = "│ ",
+                none = "  ",
+            },
+        },
+    },
+    hijack_directories = {
         enable = true,
         auto_open = true,
     },
-    -- show lsp diagnostics in the signcolumn
+    update_focused_file = {
+        enable = false,
+        update_cwd = false,
+        ignore_list = {},
+    },
+    ignore_ft_on_setup = {},
+    system_open = {
+        cmd = nil,
+        args = {},
+    },
     diagnostics = {
-        enable = true,
+        enable = false,
+        show_on_dirs = false,
         icons = {
             hint = "",
             info = "",
             warning = "",
             error = "",
-        }
-    },
-    -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-    update_focused_file = {
-        -- enables the feature
-        enable      = true,
-        -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-        -- only relevant when `update_focused_file.enable` is true
-        update_cwd  = true,
-        -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-        -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-        ignore_list = {}
-    },
-    -- configuration options for the system open command (`s` in the tree by default)
-    system_open = {
-        -- the command to run this, leaving nil should work in most cases
-        cmd  = nil,
-        -- the command arguments as a list
-        args = {}
+        },
     },
     filters = {
-        dotfiles = true,
-        custom = {'.git', '.cache', '__pycache__'}
+        dotfiles = false,
+        custom = {},
+        exclude = {},
     },
-    view = {
-        -- width of the window, can be either a number (columns) or a string in `%`
-        width = 40,
-        hide_root_folder = false,
-        -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
-        side = 'left',
-        -- if true the tree will resize itself after opening a file
-        auto_resize = false,
-        mappings = {
-            -- custom only false will merge the list with the default mappings
-            -- if true, it will only use your list to set the mappings
-            custom_only = false,
-            -- list of mappings to set on the tree manually
-            list = {}
-        },
-        number = false,
-        relativenumber = false,
-        signcolumn = 'yes',
-    },
-    trash = {
-        cmd = 'trash',
-        require_confirm = true,
+    git = {
+        enable = true,
+        ignore = true,
+        timeout = 400,
     },
     actions = {
+        use_system_clipboard = true,
         change_dir = {
             enable = true,
             global = false,
         },
         open_file = {
-            quit_on_open = true,
+            quit_on_open = false,
             resize_window = false,
             window_picker = {
                 enable = true,
                 chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
                 exclude = {
-                    filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame", },
-                    buftype  = { "nofile", "terminal", "help", },
-                }
-            }
-        }
-    }
-})
+                    filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+                    buftype = { "nofile", "terminal", "help" },
+                },
+            },
+        },
+    },
+    trash = {
+        cmd = "trash",
+        require_confirm = true,
+    },
+    log = {
+        enable = false,
+        truncate = false,
+        types = {
+            all = false,
+            config = false,
+            copy_paste = false,
+            git = false,
+            profile = false,
+        },
+    },
+}
