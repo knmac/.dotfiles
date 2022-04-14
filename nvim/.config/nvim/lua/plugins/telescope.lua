2f992d005fef67851160ff058c259627bba4bf9b
Khoi-Nguyen C. Mac <knmac@users.noreply.github.com>
2022-04-14 16:40:53 -0700

    Update telescope.lua

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -15,7 +15,36 @@ require('telescope').setup({
                 ["<C-j>"] = "preview_scrolling_down",
                 ["<C-k>"] = "preview_scrolling_up",
             }
-        }
+        },
+        vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+        },
+        prompt_prefix = "   ",
+        selection_caret = "  ",
+        entry_prefix = "  ",
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        layout_config = {
+            horizontal = {
+                prompt_position = "top",
+                preview_width = 0.55,
+                results_width = 0.8,
+            },
+            vertical = {
+                mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+        },
     },
     pickers = {
         -- Default configuration for builtin pickers goes here:
