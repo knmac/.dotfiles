3195cba534b22540fcf148fc254af8a76f3e15aa
Khoi-Nguyen C. Mac <knmac@users.noreply.github.com>
2022-04-18 16:24:45 -0700

    Update lualine.lua

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -30,7 +30,7 @@ require('lualine').setup({
             },
             'diagnostics'
         },
-        lualine_c = {'filename'},
+        lualine_c = {{'filename', path=1,}},
         lualine_x = {'encoding', 'fileformat', 'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
