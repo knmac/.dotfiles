34da276ae41763ecbe40fc72e0bab72d502cecd1
knmac <khoinguyenx@gmail.com>
2022-04-24 16:12:13 -0700

    update

--- nvim/.config/nvim/lua/plugins/toggleterm-nvim.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm-nvim.lua
@@ -9,7 +9,7 @@ require('toggleterm').setup({
         if term.direction == 'horizontal' then
             return 15
         elseif term.direction == 'vertical' then
-            return vim.o.columns * 0.4
+            return vim.o.columns * 0.3
         end
     end,
     open_mapping = [[<c-\>]],
