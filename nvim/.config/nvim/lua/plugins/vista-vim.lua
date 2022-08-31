db57db474e3234dde44cbd2c83e4941e645f3638
Khoi-Nguyen Mac <knmac@amazon.com>
2022-08-30 19:05:05 -0700

    update

--- nvim/.config/nvim/lua/plugins/vista-vim.lua
+++ nvim/.config/nvim/lua/plugins/vista-vim.lua
@@ -4,8 +4,8 @@
 -------------------------------------------------------------------------------
 local g = vim.g
 
-g.vista_default_executive = 'ctags'
+g.vista_default_executive = 'nvim_lsp'
 -- g.vista_default_executive = 'nvim_lsp'
-g.vista_finder_alternative_executives = {'nvim_lsp',}
+g.vista_finder_alternative_executives = {'ctags',}
 g.vista_sidebar_width = 40
 g.vista_disable_statusline = 1
