a482438ec07baff0101226c44ddeccf6e02ed074
Khoi-Nguyen Mac <knmac@amazon.com>
2022-10-05 16:09:39 -0700

    udpate

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -47,7 +47,7 @@ lualine.setup({
     sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { { 'filename', path = 1, } },
+        lualine_c = { { 'filename', path = 3, } },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
