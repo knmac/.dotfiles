f1677b765360c80b9da13e2353afb99e1a286431
Khoi-Nguyen Mac <knmac@amazon.com>
2022-10-24 13:01:51 -0700

    uddate

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -7,156 +7,26 @@ if not ok then return end
 
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 nvim_tree.setup({
-    auto_reload_on_write = true,
-    create_in_closed_folder = false,
-    disable_netrw = false,
-    hijack_cursor = false,
-    hijack_netrw = true,
-    hijack_unnamed_buffer_when_opening = false,
-    ignore_buffer_on_setup = false,
-    open_on_setup = false,
-    open_on_setup_file = false,
-    open_on_tab = false,
-    sort_by = 'name',
-    update_cwd = false,
     respect_buf_cwd = true,
     view = {
         width = 40,
-        hide_root_folder = false,
-        side = 'left',
-        preserve_window_proportions = false,
-        number = false,
-        relativenumber = false,
-        signcolumn = 'yes',
-        mappings = {
-            custom_only = false,
-            list = {
-                -- user mappings go here
-            },
-        },
     },
     renderer = {
-        add_trailing = true,
-        group_empty = true,
-        highlight_git = true,
-        highlight_opened_files = 'all',  -- 0: none, 1: icon, 2: name, 3: all
-        root_folder_modifier = ':~',
+        add_trailing = true, -- Append a trailing slash to folder names
+        group_empty = true, -- Compact folders that only contain a single folder into one node
+        highlight_git = true, -- Enable highlight for git attributes using NvimTreeGit*
+        highlight_opened_files = 'all', -- 0: none, 1: icon, 2: name, 3: all
         indent_markers = {
-            enable = true,
-            icons = {
-                corner = '└ ',
-                edge = '│ ',
-                none = '  ',
-            },
+            enable = true, -- Display indent markers when folders are open
         },
         icons = {
-            webdev_colors = true,
-            git_placement = 'before',
-            padding = ' ',
-            symlink_arrow = ' -> ',
-            show = {
-                file = true,
-                folder = true,
-                folder_arrow = true,
-                git = true,
-            },
-            glyphs = {
-                default = '',
-                symlink = '',
-                folder = {
-                    arrow_closed = "",
-                    arrow_open = "",
-                    default = "",
-                    open = "",
-                    empty = "",
-                    empty_open = "",
-                    symlink = "",
-                    symlink_open = "",
-                },
-                git = {
-                    unstaged = '✗',
-                    staged = '✓',
-                    unmerged = '',
-                    renamed = '➜',
-                    untracked = '★',
-                    deleted = '',
-                    ignored = '◌'
-                },
-            },
+            webdev_colors = true, -- Use the webdev icon colors, otherwise NvimTreeFileIcon
+            git_placement = 'after', -- Place where the git icons will be rendered
+            symlink_arrow = ' -> ', -- Used as a separator between symlinks' source and target
         },
-        special_files = {'Cargo.toml', 'Makefile', 'README.md', 'readme.md'},
-    },
-    hijack_directories = {
-        enable = true,
-        auto_open = true,
-    },
-    update_focused_file = {
-        enable = false,
-        update_cwd = false,
-        ignore_list = {},
-    },
-    ignore_ft_on_setup = {},
-    system_open = {
-        cmd = '',
-        args = {},
+        special_files = { 'Cargo.toml', 'Makefile', 'README.md', 'readme.md', 'environment.yml' }, -- A list of filenames that gets highlighted with NvimTreeSpecialFile
     },
     diagnostics = {
-        enable = false,
-        show_on_dirs = false,
-        icons = {
-            hint = '',
-            info = '',
-            warning = '',
-            error = '',
-        },
-    },
-    filters = {
-        dotfiles = false,
-        custom = {},
-        exclude = {},
-    },
-    git = {
-        enable = true,
-        ignore = true,
-        timeout = 400,
-    },
-    actions = {
-        use_system_clipboard = true,
-        change_dir = {
-            enable = true,
-            global = false,
-            restrict_above_cwd = false,
-        },
-        open_file = {
-            quit_on_open = false,
-            resize_window = false,
-            window_picker = {
-                enable = true,
-                chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
-                exclude = {
-                    filetype = { 'notify', 'packer', 'qf', 'diff', 'fugitive', 'fugitiveblame' },
-                    buftype = { 'nofile', 'terminal', 'help' },
-                },
-            },
-        },
-    },
-    trash = {
-        cmd = 'trash',
-        require_confirm = true,
-    },
-    live_filter = {
-        prefix = "[FILTER]: ",
-        always_show_folders = true,
-    },
-    log = {
-        enable = false,
-        truncate = false,
-        types = {
-            all = false,
-            config = false,
-            copy_paste = false,
-            git = false,
-            profile = false,
-        },
+        enable = true, -- Show LSP and COC diagnostics in the signcolumn
     },
 })
