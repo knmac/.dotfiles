d5055e99df76646e2b56500e82b357eb2834b03c
Khoi-Nguyen Mac <knmac@amazon.com>
2022-12-07 10:41:15 -0800

    update

--- nvim/.config/nvim/lua/plugins/zen-mode.lua
+++ nvim/.config/nvim/lua/plugins/zen-mode.lua
@@ -5,9 +5,12 @@
 local ok, zenmode = pcall(require, 'zen-mode')
 if not ok then return end
 
+local map = vim.keymap.set
+local default_opts = { noremap = true, silent = true }
+
 zenmode.setup({
     window = {
-        backdrop = 0.9,
+        backdrop = 0.95,
         width = 150,
     },
     plugins = {
@@ -16,4 +19,14 @@ zenmode.setup({
             font = '+4',
         },
     },
+    -- callback where you can add custom code when the Zen window opens
+    on_open = function(win)
+        map('n', 'j', 'jzz', default_opts)
+        map('n', 'k', 'kzz', default_opts)
+    end,
+    -- callback where you can add custom code when the Zen window closes
+    on_close = function()
+        map('n', 'j', 'j', default_opts)
+        map('n', 'k', 'k', default_opts)
+    end,
 })
