bfddb06bf2a9ef37211357055a6ff9e8d0982454
Khoi-Nguyen Mac <knmac@amazon.com>
2023-03-12 13:17:21 -0700

    update

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,33 @@
+local wezterm = require('wezterm')
+
+local config = {
+    -- Font
+    font = wezterm.font_with_fallback {
+        'JetBrainsMono Nerd Font',
+        'JetBrains Mono',
+    },
+    font_size = 19.0,
+    -- Color
+    color_scheme = 'Catppuccin Macchiato',
+    -- Window
+    window_background_opacity = 0.9,
+    window_padding = {
+        left = '1cell',
+        right = '1cell',
+        top = '0.5cell',
+        bottom = '0cell',
+    },
+    -- Tabs
+    use_fancy_tab_bar = true,
+    hide_tab_bar_if_only_one_tab = true,
+    -- Bell
+    audible_bell = 'Disabled',
+    visual_bell = {
+        fade_in_function = 'EaseIn',
+        fade_in_duration_ms = 150,
+        fade_out_function = 'EaseOut',
+        fade_out_duration_ms = 150,
+    },
+}
+
+return config
