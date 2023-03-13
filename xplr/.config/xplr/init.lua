f162fa302f007a705102475341c1ea40e9a24cf8
Khoi-Nguyen Mac <knmac@amazon.com>
2023-03-13 09:58:33 -0700

    update

--- xplr/.config/xplr/init.lua
+++ xplr/.config/xplr/init.lua
@@ -43,9 +43,9 @@ os.execute(
 require("xpm").setup({
     plugins = {
         -- Let xpm manage itself
-        'dtomvan/xpm.xplr',
-        'sayanarijit/fzf.xplr',
-        'sayanarijit/dual-pane.xplr',
+        "dtomvan/xpm.xplr",
+        "sayanarijit/fzf.xplr",
+        "sayanarijit/dual-pane.xplr",
     },
     auto_install = true,
     auto_cleanup = true,
@@ -53,77 +53,108 @@ require("xpm").setup({
 
 
 -- Icons ----------------------------------------------------------------------
+local function meta_icon(my_icon)
+    return { meta = { icon = my_icon } }
+end
+
 -- Common icons
 xplr.config.node_types.directory.meta.icon = " "
 xplr.config.node_types.file.meta.icon      = " "
-xplr.config.node_types.symlink.meta.icon   = " "
+xplr.config.node_types.symlink.meta.icon   = " "
 
 -- Icons based on special file name
 xplr.config.node_types.special             = {
     -- Directories
-    ["AmazonWorkDocsCompanion"] = { meta = { icon = " " } },
-    ["Applications"]            = { meta = { icon = " " } },
-    ["Desktop"]                 = { meta = { icon = " " } },
-    ["Documents"]               = { meta = { icon = " " } },
-    ["Downloads"]               = { meta = { icon = " " } },
-    ["Dropbox"]                 = { meta = { icon = " " } },
-    ["Library"]                 = { meta = { icon = " " } },
-    ["Movies"]                  = { meta = { icon = " " } },
-    ["Music"]                   = { meta = { icon = " " } },
-    ["Pictures"]                = { meta = { icon = " " } },
-    ["Public"]                  = { meta = { icon = " " } },
-    ["Templates"]               = { meta = { icon = " " } },
-    ["Videos"]                  = { meta = { icon = " " } },
-    ["datasets"]                = { meta = { icon = " " } },
-    ["workspace"]               = { meta = { icon = " " } },
-    ["workspaces"]              = { meta = { icon = " " } },
-    [".dotfiles"]               = { meta = { icon = " " } },
-    [".steam"]                  = { meta = { icon = " " } },
+    ["AmazonWorkDocsCompanion"] = meta_icon(" "),
+    ["Amazon WorkDocs Drive"]   = meta_icon(" "),
+    ["Applications"]            = meta_icon(" "),
+    ["Desktop"]                 = meta_icon(" "),
+    ["Documents"]               = meta_icon(" "),
+    ["Downloads"]               = meta_icon(" "),
+    ["Dropbox"]                 = meta_icon(" "),
+    ["Drop Box"]                = meta_icon(" "),
+    ["Library"]                 = meta_icon(" "),
+    ["Movies"]                  = meta_icon(" "),
+    ["Music"]                   = meta_icon(" "),
+    ["Pictures"]                = meta_icon(" "),
+    ["Public"]                  = meta_icon(" "),
+    ["Templates"]               = meta_icon(" "),
+    ["Videos"]                  = meta_icon(" "),
+    ["datasets"]                = meta_icon(" "),
+    ["workspace"]               = meta_icon(" "),
+    ["workspaces"]              = meta_icon(" "),
+    [".dotfiles"]               = meta_icon(" "),
+    [".steam"]                  = meta_icon(" "),
     -- Files
-    ["CMakeLists.txt"]          = { meta = { icon = " " } },
-    [".DS_Store"]               = { meta = { icon = " " } },
-    [".Dockerfile"]             = { meta = { icon = " " } },
-    [".gitlab-ci.yml"]          = { meta = { icon = " " } },
-    [".git"]                    = { meta = { icon = " " } },
+    ["Makefile"]                = meta_icon(" "),
+    ["README"]                  = meta_icon(" "),
+    [".DS_Store"]               = meta_icon(" "),
+    [".Dockerfile"]             = meta_icon(" "),
+    [".gitlab-ci.yml"]          = meta_icon(" "),
+    [".git"]                    = meta_icon(" "),
+    ["CMakeLists.txt"]          = meta_icon(" "),
+    [".bashrc"]                 = meta_icon(" "),
+    [".bashprofile"]            = meta_icon(" "),
+    [".zshrc"]                  = meta_icon(" "),
+    [".zshenv"]                 = meta_icon(" "),
+    [".zprofile"]               = meta_icon(" "),
 }
 
--- Icons by file type
+-- Icons by MIME list
 xplr.config.node_types.mime_essence        = {
     application = {
-        ["*"]                                                             = { meta = { icon = " " } },
-        ["pdf"]                                                           = { meta = { icon = " " } },
+        ["*"]                                                             = meta_icon(" "),
+        ["pdf"]                                                           = meta_icon(" "),
+        ["vnd.amazon.ebook"]                                              = meta_icon(" "),
+        ["octet-stream"]                                                  = meta_icon(" "),
+        ["x-sh"]                                                          = meta_icon(" "),
+        ["rtf"]                                                           = meta_icon(" "),
         -- word
-        ["msword"]                                                        = { meta = { icon = " " } },
-        ["vnd.openxmlformats-officedocument.wordprocessingml.document"]   = { meta = { icon = " " } },
-        ["vnd.oasis.opendocument.text"]                                   = { meta = { icon = " " } },
+        ["msword"]                                                        = meta_icon(" "),
+        ["vnd.openxmlformats-officedocument.wordprocessingml.document"]   = meta_icon(" "),
+        ["vnd.oasis.opendocument.text"]                                   = meta_icon(" "),
         -- excel
-        ["vnd.ms-excel"]                                                  = { meta = { icon = " " } },
-        ["vnd.openxmlformats-officedocument.spreadsheetml.sheet"]         = { meta = { icon = " " } },
-        ["vnd.oasis.opendocument.spreadsheet"]                            = { meta = { icon = " " } },
+        ["vnd.ms-excel"]                                                  = meta_icon(" "),
+        ["vnd.openxmlformats-officedocument.spreadsheetml.sheet"]         = meta_icon(" "),
+        ["vnd.oasis.opendocument.spreadsheet"]                            = meta_icon(" "),
         -- powerpoint
-        ["vnd.ms-powerpoint"]                                             = { meta = { icon = " " } },
-        ["vnd.openxmlformats-officedocument.presentationml.presentation"] = { meta = { icon = " " } },
-        ["vnd.oasis.opendocument.presentation"]                           = { meta = { icon = " " } },
+        ["vnd.ms-powerpoint"]                                             = meta_icon(" "),
+        ["vnd.openxmlformats-officedocument.presentationml.presentation"] = meta_icon(" "),
+        ["vnd.oasis.opendocument.presentation"]                           = meta_icon(" "),
+        -- archive
+        ["gzip"]                                                          = meta_icon(" "),
+        ["java-archive"]                                                  = meta_icon(" "),
+        ["vnd.rar"]                                                       = meta_icon(" "),
+        ["x-7z-compressed"]                                               = meta_icon(" "),
+        ["x-bzip"]                                                        = meta_icon(" "),
+        ["x-bzip2"]                                                       = meta_icon(" "),
+        ["x-freearc"]                                                     = meta_icon(" "),
+        ["x-tar"]                                                         = meta_icon(" "),
+        ["zip"]                                                           = meta_icon(" "),
     },
     text = {
-        ["*"] = { meta = { icon = " " } },
-        html  = { meta = { icon = " " } },
-        css   = { meta = { icon = " " } },
+        ["*"]    = meta_icon(" "),
+        ["html"] = meta_icon(" "),
+        ["css"]  = meta_icon(" "),
     },
     image = {
-        ["*"] = { meta = { icon = " " } },
+        ["*"] = meta_icon(" "),
     },
     video = {
-        ["*"] = { meta = { icon = " " } },
+        ["*"] = meta_icon(" "),
     },
     audio = {
-        ["*"] = { meta = { icon = " " } },
+        ["*"] = meta_icon(" "),
     },
     font = {
-        ["*"] = { meta = { icon = " " } },
+        ["*"] = meta_icon(" "),
     },
 }
 
-xplr.config.node_types.extension = {
-    ["py"] = { meta = { icon = " " } },
+-- Icons by other extension
+xplr.config.node_types.extension           = {
+    ["py"]    = meta_icon(" "),
+    ["ipynb"] = meta_icon(" "),
+    ["zsh"]   = meta_icon(" "),
+    ["bash"]  = meta_icon(" "),
 }
