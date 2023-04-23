0817cb23d8d9d29331068960988371565d7e4c46
Khoi-Nguyen Mac <knmac@amazon.com>
2023-04-23 14:27:58 -0700

    update

---
+++ wezterm/.config/wezterm/enable_undercurl.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+tempfile=$(mktemp) \
+  && curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/master/termwiz/data/wezterm.terminfo \
+  && tic -x -o ~/.terminfo $tempfile \
+  && rm $tempfile
+
+echo 'TERM="wezterm"' > ~/.zshrc
