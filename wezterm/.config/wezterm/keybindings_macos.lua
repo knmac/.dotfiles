4e4f792f19441f6d690d31c25b86f905ca914d02
Khoi-Nguyen Mac <knmac@amazon.com>
2023-06-01 18:56:01 -0700

    update

--- wezterm/.config/wezterm/keybindings_macos.lua
+++ wezterm/.config/wezterm/keybindings_macos.lua
@@ -162,6 +162,21 @@ local keys = {
     { key = 'DownArrow',  mods = 'SHIFT|ALT|CTRL', action = act.AdjustPaneSize { 'Down', 1 } },
     { key = 'Copy',       mods = 'NONE',           action = act.CopyTo 'Clipboard' },
     { key = 'Paste',      mods = 'NONE',           action = act.PasteFrom 'Clipboard' },
+    {
+        key = 'E',
+        mods = 'CTRL|SHIFT',
+        action = act.PromptInputLine {
+            description = 'Enter new name for tab',
+            action = wezterm.action_callback(function(window, pane, line)
+                -- line will be `nil` if they hit escape without entering anything
+                -- An empty string if they just hit enter
+                -- Or the actual line of text they wrote
+                if line then
+                    window:active_tab():set_title(line)
+                end
+            end),
+        },
+    },
 }
 local key_tables = {
     copy_mode = {
