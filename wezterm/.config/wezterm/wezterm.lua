cf6f01093ec35a7fdf2f65c5e6f6801475ef9f91
Khoi-Nguyen Mac <khoinguyenx@gmail.com>
2023-11-20 21:47:10 -0800

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -36,7 +36,7 @@ config.color_scheme = 'Catppuccin Macchiato'
 config.window_background_opacity = 0.9
 config.window_padding = {
     left = '1cell',
-    right = '1cell',
+    right = '0.5cell',
     top = '0.5cell',
     bottom = '0cell',
 }
