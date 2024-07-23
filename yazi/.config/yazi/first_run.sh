8909f33a0a39bb11b52b4761412eeb8afa1f84e4
Khoi-Nguyen Mac <knmac@amazon.com>
2024-07-22 20:29:03 -0700

    add instruction to setup yy

--- yazi/.config/yazi/first_run.sh
+++ yazi/.config/yazi/first_run.sh
@@ -6,3 +6,18 @@ fi
 if [ ! -d "plugins" ]; then
     git clone https://github.com/yazi-rs/plugins
 fi
+
+# Configure yazi
+echo "Add the following to your .bashrc or .zshrc etc."
+printf '
+function yy() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+        # cd -- "$cwd"
+        "$cwd"
+    fi
+    rm -f -- "$tmp"
+}
+bindkey -s "^o" "yy\n"  # set up key-binding
+'
